ce6f8dc7100af6c8e0bdc2732004e93970cb28e6
ibrahim-sert <evo3506@gmail.com>
2023-04-07 11:08:41 +0300

    global typess tarted

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,29 @@ import InputForm from "../components/InputForm";
 import TodoList from "../components/TodoList";
 
 const url = "https://63ef8796271439b7fe70b81b.mockapi.io/users";
+
 const Home = () => {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+
+  const getTodos = async () => {
+    try {
+      const { data } = await axios.get<TodoType[]>(url);
+      console.log(data);
+
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
     <div className="main">
       <InputForm />
-      <TodoList />
+      <TodoList todos={todos} />
     </div>
   );
 };
